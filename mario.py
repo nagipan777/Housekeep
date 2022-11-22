26fe20734fb4c234ed54a469e4deb12bcec6e5b9
nagipan777 <nagipan777@users.noreply.github.com>
2022-11-22 05:55:47 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,15 +1,15 @@
 # TODO
 from cs50 import get_int
-## loop for input height size
+# loop for input height size
 while (True):
     h = get_int("Height:")
     if (h < 1 or h > 8):
         h = get_int("Height:")
     if (h >= 1 or h <= 8):
         break
-## for loop row " " and "#"
+# for loop row " " and "#"
 for i in range(1, h + 1):
     print(" " * (h - i), end="")
     print("#" * i, end="")
-    print("  ",end="")
-    print("#" * i,end="\n")
+    print("  ", end="")
+    print("#" * i, end="\n")
